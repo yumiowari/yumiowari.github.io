95ca5347327c3cb7cb22afd54c2f4699d7cc6547
Yumiowari <owariyumi@gmail.com>
2025-02-24 16:57:18 -0300

    [FIX] Padrão de código

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     const signature = document.getElementById("signature");
-    const originalText = signature.textContent;
+    const original_text = signature.textContent;
     let interval;
 
     signature.addEventListener("mouseover", () => {
-        let currentText = originalText;
+        let current_text = original_text;
         let index = 0;
 
         interval = setInterval(() => {
-            currentText = currentText.slice(-1) + currentText.slice(0, -1); // "rotaciona" o texto
-            signature.textContent = currentText;
+            current_text = current_text.slice(-1) + current_text.slice(0, -1); // "rotaciona" o texto
+            signature.textContent = current_text;
             index++;
 
-            if(index >= originalText.length)index = 0;
+            if(index >= original_text.length)index = 0;
         }, 200); // tempo entre os frames (em milissegundos)
     });
 
     signature.addEventListener("mouseout", () => {
         clearInterval(interval); // interrompe a animação
-        signature.textContent = originalText; // restaura o texto original
+        signature.textContent = original_text; // restaura o texto original
     });
 });
